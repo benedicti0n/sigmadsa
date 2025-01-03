86d17e38e070f8871589dda642b0b1d8cbb9a00d
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-03 14:48:38 +0530

    credentialsProvder and Github provider add

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,29 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials"
 
 const handler = NextAuth({
     providers: [
+        CredentialsProvider({
+            name: "Signup with your email and password",
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "username" },
+                password: { label: "Password", type: "password", placeholder: "Password" },
+                confirmPassword: { label: "Confirm Password", type: "password", placeholder: "Confirm Password" },
+            },
+            // async authorize(credentials, req) {
+
+            // }
+        }),
         GoogleProvider({
             clientId: "aa",
             clientSecret: "aa"
-        })
+        }),
+        GithubProvider({
+            clientId: "aa",
+            clientSecret: "aa"
+        }),
     ]
 })
 
